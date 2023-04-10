1599460d117995ca2e2011e800aa6e3ddee73a2a
saulo.maker <saulosousasilva3@gmail.com>
2023-04-10 20:58:55 -0300

    Deleted classes referring to Javafx

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.saulodev.pedidoksb {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.saulodev.Singleton to javafx.fxml;
-    exports com.saulodev.Singleton;
-}
